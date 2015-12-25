f7ddef045a3d85d197dbaa6debb88e70aae6a130
ngyuki <ngyuki.jp@gmail.com>
2015-12-25 15:48:37 +0900

    モックで archive や unarchive の URL が /archive のようになってしまうのを修正

--- html/js/main.js
+++ html/js/main.js
@@ -5,7 +5,7 @@ $(function(){
         var $parent = $(this).parents('[data-item-id]');
         var item_id = $parent.data('item-id');
 
-        var action = $parent.hasClass('archive') ? "/unarchive" : "/archive";
+        var action = $parent.hasClass('archive') ? "unarchive" : "archive";
 
         $.post(action, { item_id:item_id }).success(function(){
             $parent.toggleClass('archive');
